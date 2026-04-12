950d48aaaed7345194b866f6d2f08ccdc748885f
dhonicsk-dev <malikmskabdul.com>
2026-04-12 15:33:10 +0530

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 
-const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
-const app = express();
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+app.options("*", cors());
 
 /* ================== MIDDLEWARE ================== */
 app.use(express.json());
