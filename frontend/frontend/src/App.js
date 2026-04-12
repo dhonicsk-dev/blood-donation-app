7b5f977801f2a4e2487159b8c3779cd380e857a4
dhonicsk-dev <malikmskabdul.com>
2026-04-12 18:19:27 +0530

    fixed blank screen issue

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,18 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
-import Register from "./Register";
-import Dashboard from "./Dashboard";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Login />;
 }
 
 export default App;
