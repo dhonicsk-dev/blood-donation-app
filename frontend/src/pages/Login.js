8b8b61d45fc9e00021bdb980b34d9d07ea8e0319
dhonicsk-dev <malikmskabdul.com>
2026-04-14 23:11:02 +0530

    fix login API URL

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -13,7 +13,8 @@ function Login() {
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/login",
+        "const API = process.env.REACT_APP_API_URL;",
+        await axios.post(`${API}/api/login`, form);
         form
       );
 
